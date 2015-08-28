a82f2f4952f33cd1692cb759ae149a63c2009e5a
GLNRO <lg@glnro.com>
2015-08-28 14:48:31 -0400

    add user model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+	include BCrypt
+	has_secure_password
+
+	has_many :created_surveys, class_name: :Survey, foreign_key: :creator_id
+	has_many :taken_surveys, foreign_key: :taker_id
+	has_many :responses
+
+	validates_presence_of :username, :email, :password
+	validates_uniqueness_of :username
 end
