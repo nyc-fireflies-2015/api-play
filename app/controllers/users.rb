0b71273d608e34fa4ba20e6ca3adedca284da85a
aderend <d.dogrusoy@gmail.com>
2015-08-28 19:26:58 -0400

    header partial, fix helper

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -14,6 +14,11 @@ post '/users' do
   end
 end
 
+get '/users/:id' do
+  @user = User.find_by(id: params[:id])
+  erb :'/users/show'
+end
+
 get '/users/edit' do
   @user = User.find_by(id: session[:user_id])
   erb :'/users/edit'
