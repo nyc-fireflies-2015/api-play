98dabd9d1c6121d783e0ed716b187ebc046df1ee
aderend <d.dogrusoy@gmail.com>
2015-08-28 18:39:10 -0400

    user edit profile

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -3,8 +3,9 @@ get '/login' do
 end
 
 post '/login' do
-  @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
-  if @user
+  user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+  if user
+    session[:user_id] = user.id
     redirect '/'
   else
     #add flash error
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -4,12 +4,24 @@ get '/signup' do
 end
 
 post '/users' do
-  @user = User.new(params[:user])
-  # binding.pry
-  if @user.save
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
     redirect to'/'
   else
     #add flash error
     redirect '/fail'
   end
+end
+
+get '/users/edit' do
+  @user = User.find_by(id: session[:user_id])
+  erb :'/users/edit'
+end
+
+put '/users' do
+  @user = User.find_by(id: session[:user_id])
+  @user.update_attributes(params[:user])
+  redirect '/'
+
 end
