d7ab2157e3ce5432f2b7b4ed9e59b29867be7037
GLNRO <lg@glnro.com>
2015-08-28 17:09:26 -0400

    Create question associations & validations tests

--- spec/features/associations_spec.rb
+++ spec/features/associations_spec.rb
@@ -44,3 +44,18 @@ describe Survey do
 		should validate_presence_of(:category)
 	end
 end
+
+describe Question do
+
+	it 'should belong to a survey' do
+		should belong_to(:survey)
+	end
+
+	it 'should have many choices' do
+		should have_many(:choices)
+	end
+
+	it 'should validate the presence of body' do
+		should validate_presence_of(:body)
+	end
+end
