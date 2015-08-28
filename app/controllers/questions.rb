0d1f70e736b60d732490a58b0fdfbbfaac21ff4a
Leah Goldberg <email": null>
2015-08-28 17:22:57 -0400

    add controller routes for questions

---
+++ app/controllers/questions.rb
@@ -0,0 +1,22 @@
+get '/questions/new' do
+  @survey = Survey.find_by(id: params[:survey][:id])
+  @question = Question.new
+  "error" unless @survey && @question
+  erb :'questions/new'
+end
+
+get '/questions/:id' do
+  @question = Question.find_by(id: params[:id])
+  @taken_survey = TakenSurvey.find_by(id: params[:taken_survey][:id])
+  "error" unless @question && @taken_survey
+  @choices = @question.choices
+  erb :'questions/show'
+end
+
+post '/questions' do
+  @survey = Survey.find_by(id: params[:survey][:id])
+  @question = Question.new(survey: @survey, body: params[:question][:body])
+  "error" unless @survey && @question.save
+  @survey.questions << @question
+  redirect '/choices/new'
+end
