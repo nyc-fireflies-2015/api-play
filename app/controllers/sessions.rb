bec9e2cac0157e7f131c02118dbeab04cffbfb23
aderend <d.dogrusoy@gmail.com>
2015-08-28 17:55:48 -0400

    add logout and login auth

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -3,5 +3,16 @@ get '/login' do
 end
 
 post '/login' do
-  @user = User.find_by(username: params[:user])
+  @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+  if @user
+    redirect '/'
+  else
+    #add flash error
+    redirect '/fail'
+  end
+end
+
+delete '/logout' do
+  session.clear
+  redirect '/logged_out'
 end
--- app/controllers/welcome.rb
+++ app/controllers/welcome.rb
@@ -1,3 +1,3 @@
 get '/' do
-  "welcome to Survey Pony"
+  erb :'/home'
 end
