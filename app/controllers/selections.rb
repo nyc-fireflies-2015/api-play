f2357f5f915dcb11a0c51f1660744d09df1bc42b
Leah Goldberg <email": null>
2015-08-29 10:57:09 -0400

    finish views for taken_survey

--- app/controllers/selections.rb
+++ app/controllers/selections.rb
@@ -2,7 +2,6 @@ post '/selections' do
   @taken_survey = TakenSurvey.find_by(id: params[:taken_survey_id])
   @choice = Choice.find_by(id: params[:choice_id])
   @selection = Selection.new(taken_survey: @taken_survey, user: current_user, choice: @choice)
-  # binding.pry
   "error" unless @selection.save
   @next_question = @taken_survey.survey.next_question(@choice.question)
   redirect "/takensurveys/#{@taken_survey.id}" if @next_question.nil?
