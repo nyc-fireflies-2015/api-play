9d1e1226f4ac46ff9b002a0b3fab5b703cfde767
Leah Goldberg <email": null>
2015-08-29 12:55:22 -0400

    fix choices so all inputs added to question

--- app/controllers/choices.rb
+++ app/controllers/choices.rb
@@ -7,10 +7,5 @@ end
 post '/choices' do
   @question = Question.find_by(id: params[:question_id])
   @question.add_choices(params[:choice])
-  binding.pry
-  # params[:choice].each do |index, choice|
-  #   new_choice = @question.choices.build(body: choice[:body])
-  #   "error" unless new_choice.save
-  # end
   redirect "/surveys/#{@question.survey.id}/questions/new"
 end
