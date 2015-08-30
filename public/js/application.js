e5e4af0f4b57fa22920ecc120c6e09535489e823
GLNRO <lg@glnro.com>
2015-08-30 14:38:10 -0400

    Prevent start survey default action

--- public/js/application.js
+++ public/js/application.js
@@ -2,4 +2,12 @@ $('document').ready(function(){
 
   console.log("hellpo");
 
+  $('#start_survey').on("submit",function(event){
+      var target = $(event.target);
+      event.preventDefault();
+      console.log("we stopped it")
+
+  });
+
+
 });
