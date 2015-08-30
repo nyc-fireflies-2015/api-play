f9202a1af34af3ab58a27e113c69c5478f36ebc0
GLNRO <lg@glnro.com>
2015-08-30 15:56:28 -0400

    Edit next question placement in the controller

--- app/controllers/selections.rb
+++ app/controllers/selections.rb
@@ -2,11 +2,12 @@ post '/selections' do
   @taken_survey = TakenSurvey.find_by(id: params[:taken_survey_id])
   @choice = Choice.find_by(id: params[:choice_id])
   @selection = Selection.new(taken_survey: @taken_survey, user: current_user, choice: @choice)
-  next_question = @taken_survey.survey.next_question(@choice.question)
   if @selection.save
     if request.xhr?
+    next_question = @taken_survey.survey.next_question(@choice.question)
       erb :"/questions/_draw_question", layout: false, locals: {taken_survey: @taken_survey, question: next_question}
     else
+      next_question = @taken_survey.survey.next_question(@choice.question)
       redirect "/takensurveys/#{@taken_survey.id}" if next_question.nil?
       redirect "/takensurveys/#{@taken_survey.id}/questions/#{next_question.id}"
     end
