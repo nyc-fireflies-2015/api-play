592d6aa1fb817775164649a988d20786d6376ded
GLNRO <lg@glnro.com>
2015-08-30 16:26:36 -0400

    Change target listener

--- public/js/application.js
+++ public/js/application.js
@@ -2,16 +2,15 @@ $('document').ready(function(){
 
   console.log("hellpo");
 
-  $('#run_survey').on("submit",function(event){
+  $('.question_wrapper').on("submit",function(event){
       var target = $(event.target);
       event.preventDefault();
       console.log("we stopped it")
-
       $.ajax(target.attr("action"),{
         data: target.serialize(),
         method: target.attr("method")
       }).done(function(response){
-        $("#question_container").replaceWith(response);
+        $(".question_container").replaceWith(response);
       });
 
   });
