18a316c8438238ce7a3977508ffd69fb84312a54
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-08-29 23:14:57 -0400

    Add flash errors to selection controller

--- app/controllers/selections.rb
+++ app/controllers/selections.rb
@@ -2,9 +2,12 @@ post '/selections' do
   @taken_survey = TakenSurvey.find_by(id: params[:taken_survey_id])
   @choice = Choice.find_by(id: params[:choice_id])
   @selection = Selection.new(taken_survey: @taken_survey, user: current_user, choice: @choice)
-  #flash errors
-  "error" unless @selection.save
-  next_question = @taken_survey.survey.next_question(@choice.question)
-  redirect "/takensurveys/#{@taken_survey.id}" if next_question.nil?
-  redirect "/takensurveys/#{@taken_survey.id}/questions/#{next_question.id}"
+  if @selection.save
+    next_question = @taken_survey.survey.next_question(@choice.question)
+    redirect "/takensurveys/#{@taken_survey.id}" if next_question.nil?
+    redirect "/takensurveys/#{@taken_survey.id}/questions/#{next_question.id}"
+  else
+    flash[:error] = @selection.errors.full_messages
+    redirect "/takensurveys/#{@taken_survey.id}/questions/#{params[:question_id]}"
+  end
 end
