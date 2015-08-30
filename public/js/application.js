09153702cfce959c9fc106a04f7e6a9efce3c4b2
GLNRO <lg@glnro.com>
2015-08-30 14:42:08 -0400

    Get html from ajax request

--- public/js/application.js
+++ public/js/application.js
@@ -7,6 +7,13 @@ $('document').ready(function(){
       event.preventDefault();
       console.log("we stopped it")
 
+      $.ajax(target.attr("action"),{
+        data: target.serialize(),
+        method: target.attr("method")
+      }).done(function(response){
+        console.log(response)
+      });
+
   });
 
 
