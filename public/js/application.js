8befd7aec45e6fcccaa2cad8b8679024db4e4460
GLNRO <lg@glnro.com>
2015-08-30 15:39:18 -0400

    Edit ajax request to submit on takensurveys/id/questions/id page

--- public/js/application.js
+++ public/js/application.js
@@ -2,7 +2,7 @@ $('document').ready(function(){
 
   console.log("hellpo");
 
-  $('#start_survey').on("submit",function(event){
+  $('#run_survey').on("submit",function(event){
       var target = $(event.target);
       event.preventDefault();
       console.log("we stopped it")
