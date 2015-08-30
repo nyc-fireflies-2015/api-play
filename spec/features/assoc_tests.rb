4bccb6be536273ff45842bfc5836fbc175854c3e
GLNRO <lg@glnro.com>
2015-08-30 11:41:56 -0400

    Add tests for Taken Survey git status

--- spec/features/assoc_tests.rb
+++ spec/features/assoc_tests.rb
@@ -83,3 +83,18 @@ describe Choices do
 		should validate_presence_of(:body)
 	end
 end
+
+describe TakenSurvey do
+
+	it 'should belong to a survey' do
+		should belong_to(:survey)
+	end
+
+	it 'should belong to a taker' do
+		should belong_to(:taker).class_name('User')
+	end
+
+	it 'should have many selections' do
+		should have_many(:selections)
+	end
+end
