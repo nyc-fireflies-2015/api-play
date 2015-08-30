c03b0e01f34527fa18f4b5ce08a0f63b8dd990cc
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-08-30 17:24:38 -0400

    WIP

--- app/controllers/selections.rb
+++ app/controllers/selections.rb
@@ -5,12 +5,12 @@ post '/selections' do
   next_question = @taken_survey.survey.next_question(@choice.question)
 
   if @selection.save
-      if request.xhr?
-        erb :"/taken_surveys/show", layout: false unless next_question
-        erb :"/questions/_draw_question", layout: false, locals: {taken_survey: @taken_survey, question: next_question}
-      else
-        redirect "/takensurveys/#{@taken_survey.id}/questions/#{next_question.id}"
-      end
+    if request.xhr?
+      erb :"/taken_surveys/show", layout: false unless next_question
+      erb :"/questions/_draw_question", layout: false, locals: {taken_survey: @taken_survey, question: next_question}
+    else
+      redirect "/takensurveys/#{@taken_survey.id}"
+    end
   else
     flash[:error] = @selection.errors.full_messages
     redirect "/takensurveys/#{@taken_survey.id}/questions/#{params[:question_id]}"
