8f3b52f1e6b85bb6db854ff9d35bf0af9eb284ca
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-08-30 17:50:12 -0400

    Refactor selections controller

--- app/controllers/selections.rb
+++ app/controllers/selections.rb
@@ -1,27 +1,29 @@
 post '/selections' do
-  @taken_survey = TakenSurvey.find_by(id: params[:taken_survey_id])
-  @choice = Choice.find_by(id: params[:choice_id])
-  @selection = Selection.new(taken_survey: @taken_survey, user: current_user, choice: @choice)
-  next_question = @taken_survey.survey.next_question(@choice.question)
+  taken_survey = TakenSurvey.find_by(id: params[:taken_survey_id])
+  choice = Choice.find_by(id: params[:choice_id])
+  selection = Selection.new(taken_survey: taken_survey, user: current_user, choice: choice)
+  survey = taken_survey.survey
+  taken_summary = taken_survey.taken_summary
+  next_question = survey.next_question(choice.question)
 
-  if @selection.save
+  if selection.save
     if request.xhr?
       if next_question.nil?
         erb :"/taken_surveys/_show_answers", layout: false, locals: {
-          survey: @taken_survey.survey,
-          taken_summary: @taken_survey.taken_summary
+          survey: survey,
+          taken_summary: taken_summary
           }
       else
         erb :"/questions/_draw_question", layout: false, locals: {
-          taken_survey: @taken_survey,
+          taken_survey: taken_survey,
           question: next_question
         }
       end
     else
-      redirect "/takensurveys/#{@taken_survey.id}"
+      redirect "/takensurveys/#{taken_survey.id}"
     end
   else
-    flash[:error] = @selection.errors.full_messages
-    redirect "/takensurveys/#{@taken_survey.id}/questions/#{params[:question_id]}"
+    flash[:error] = selection.errors.full_messages
+    redirect "/takensurveys/#{taken_survey.id}/questions/#{params[:question_id]}"
   end
 end
