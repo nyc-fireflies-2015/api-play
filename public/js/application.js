3e9dbe34a10abb5d967626ef1401d20841b72758
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-08-30 17:50:45 -0400

    Add fail conditional and refactor JS file

--- public/js/application.js
+++ public/js/application.js
@@ -1,19 +1,17 @@
 $('document').ready(function(){
 
-  console.log("hellpo");
-
   $('.question_wrapper').on("submit",function(event){
-      var target = $(event.target);
-      event.preventDefault();
-      console.log("we stopped it")
-      $.ajax(target.attr("action"),{
-        data: target.serialize(),
-        method: target.attr("method")
-      }).done(function(response){
-        $(".question_container").replaceWith(response);
-      });
+    var target = $(event.target);
+    event.preventDefault();
+
+    $.ajax(target.attr("action"),{
+      data: target.serialize(),
+      method: target.attr("method")
+    }).done(function(response){
+      $(".question_container").replaceWith(response);
+    }).fail(function() {
+      alert("Error")
+    });
 
   });
-
-
 });
