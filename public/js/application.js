76b86618aaaf5775017eaefd42b1c3a67a0c72ec
GLNRO <lg@glnro.com>
2015-08-30 15:57:55 -0400

    Ajax replaceWith for first two questions

--- public/js/application.js
+++ public/js/application.js
@@ -11,7 +11,7 @@ $('document').ready(function(){
         data: target.serialize(),
         method: target.attr("method")
       }).done(function(response){
-        console.log(response)
+        $("#question_container").replaceWith(response);
       });
 
   });
